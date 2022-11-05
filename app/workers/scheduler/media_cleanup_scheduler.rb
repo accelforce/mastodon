fa537af83452de6a0d540201454dd26200cca239
kyori19 <kyori@accelf.net>
2022-11-05 18:18:51 +0000

    Revert "Add automated media removal"
    This reverts commit 17b2a3c2056c27857e842fe32d7f3c6300c03950.

--- app/workers/scheduler/media_cleanup_scheduler.rb
+++ app/workers/scheduler/media_cleanup_scheduler.rb
@@ -7,25 +7,11 @@ class Scheduler::MediaCleanupScheduler
 
   def perform
     unattached_media.find_each(&:destroy)
-
-    return unless Setting.media_remove_enabled
-
-    old_media(Setting.media_remove_days).find_each do |attachment|
-      next if attachment.file.blank?
-
-      attachment.file.destroy
-      attachment.thumbnail.destroy
-      attachment.save
-    end
   end
 
   private
 
   def unattached_media
     MediaAttachment.reorder(nil).unattached.where('created_at < ?', 1.day.ago)
-  end
-
-  def old_media(n)
-    MediaAttachment.cached.remote.where('created_at < ?', n.days.ago)
   end
 end
--- spec/workers/scheduler/media_cleanup_scheduler_spec.rb
+++ spec/workers/scheduler/media_cleanup_scheduler_spec.rb
@@ -3,67 +3,13 @@ require 'rails_helper'
 describe Scheduler::MediaCleanupScheduler do
   subject { described_class.new }
 
-  let!(:old_local_media) { Fabricate(:media_attachment, account_id: nil, created_at: 10.days.ago) }
-  let!(:new_local_media) { Fabricate(:media_attachment, account_id: nil, created_at: 1.hour.ago) }
+  let!(:old_media) { Fabricate(:media_attachment, account_id: nil, created_at: 10.days.ago) }
+  let!(:new_media) { Fabricate(:media_attachment, account_id: nil, created_at: 1.hour.ago) }
 
-  let(:status) { Fabricate(:status) }
-  let!(:very_old_remote_media) { Fabricate(:media_attachment, created_at: 30.days.ago, remote_url: 'https://example.com/example.png', status: status) }
-  let!(:old_remote_media) { Fabricate(:media_attachment, created_at: 15.days.ago, remote_url: 'https://example.com/example.png', status: status) }
-  let!(:new_remote_media) { Fabricate(:media_attachment, created_at: 1.days.ago, remote_url: 'https://example.com/example.png', status: status) }
+  it 'removes old media records' do
+    subject.perform
 
-  context 'when auto removal enabled' do
-    before do
-      Setting.media_remove_enabled = true
-    end
-
-    it 'removes old local media records' do
-      subject.perform
-
-      expect { old_local_media.reload }.to raise_error(ActiveRecord::RecordNotFound)
-      expect(new_local_media.reload).to be_persisted
-    end
-
-    it 'removes old remote media records' do
-      subject.perform
-
-      expect(very_old_remote_media.reload).to be_needs_redownload
-      expect(old_remote_media.reload).to be_needs_redownload
-      expect(new_remote_media.reload).not_to be_needs_redownload
-    end
-
-    context 'when a days setting configured' do
-      before do
-        Setting.media_remove_days = 20
-      end
-
-      it 'respects a days setting' do
-        subject.perform
-
-        expect(very_old_remote_media.reload).to be_needs_redownload
-        expect(old_remote_media.reload).not_to be_needs_redownload
-        expect(new_remote_media.reload).not_to be_needs_redownload
-      end
-    end
-  end
-
-  context 'when auto removal disabled' do
-    before do
-      Setting.media_remove_enabled = false
-    end
-
-    it 'removes old local media records' do
-      subject.perform
-
-      expect { old_local_media.reload }.to raise_error(ActiveRecord::RecordNotFound)
-      expect(new_local_media.reload).to be_persisted
-    end
-
-    it 'does not remove old remote media records' do
-      subject.perform
-
-      expect(very_old_remote_media.reload).not_to be_needs_redownload
-      expect(old_remote_media.reload).not_to be_needs_redownload
-      expect(new_remote_media.reload).not_to be_needs_redownload
-    end
+    expect { old_media.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect(new_media.reload).to be_persisted
   end
 end
