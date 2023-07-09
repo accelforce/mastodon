9c079cb28450ea7aaa647d946a7ce9acbfade4ad
kyori19 <kyori@accelf.net>
2023-07-09 18:15:19 +0000

    Fix lint for deprecated branches

--- db/migrate/20220216155817_remove_private_filters.rb
+++ db/migrate/20220216155817_remove_private_filters.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemovePrivateFilters < ActiveRecord::Migration[6.1]
   def change
     up_only do
--- db/post_migrate/20221105183156_remove_media_removal.rb
+++ db/post_migrate/20221105183156_remove_media_removal.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class RemoveMediaRemoval < ActiveRecord::Migration[6.1]
   def change
     up_only do
-      Setting.where(var: %w[media_remove_enabled media_remove_days])
+      Setting.where(var: %w(media_remove_enabled media_remove_days))
              .destroy_all
     end
   end
