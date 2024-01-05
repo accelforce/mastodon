f982c834e13001a62047fe425571ded15b46fb41
kyori19 <kyori@accelf.net>
2024-01-05 07:10:50 +0000

    fix lint

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::FavouritesController < Api::BaseController
   after_action :insert_pagination_headers
 
   def index
-    @statuses   = load_statuses
+    @statuses = load_statuses
     accounts = @statuses.filter_map { |status| status.quote&.account }.uniq
     render json: @statuses, each_serializer: REST::StatusSerializer,
            relationships: StatusRelationshipsPresenter.new(@statuses, current_user&.account_id),
