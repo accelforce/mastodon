569ca4fdcb1ab04744352f8420fec3df371f29cd
kyori19 <kyori@accelf.net>
2024-01-05 07:10:21 +0000

    fix migration

--- db/migrate/20220216155817_remove_private_filters.rb
+++ db/migrate/20220216155817_remove_private_filters.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class RemovePrivateFilters < ActiveRecord::Migration[6.1]
+  class CustomFilter < ApplicationRecord; end
+
   def change
     up_only do
       CustomFilter.where('phrase LIKE \'private-filter:@%\'').destroy_all
